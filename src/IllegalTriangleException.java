cc007dc356dab4aa22db9626b0e65ca687a58850
anh.tran(c0720i1) <69283798+theanh111@users.noreply.github.com>
2020-09-29 16:36:03 +0700

    bai tap Sử dụng lớp IllegalTriangleException

--- src/IllegalTriangleException.java
+++ src/IllegalTriangleException.java
@@ -1,5 +1,2 @@
 public class IllegalTriangleException extends Exception {
-    public IllegalTriangleException(String s) {
-        super(s);
-    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,31 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.println(" === Triangle Checker ===");
-        System.out.println("Input size 1: ");
-        Double firstSide = input.nextDouble();
-        System.out.println("Input size 2: ");
-        Double secondSide = input.nextDouble();
-        System.out.println("Input size 3: ");
-        Double thirdSide = input.nextDouble();
-        Double sum1 = firstSide + secondSide;
-        Double sum2 = secondSide + thirdSide;
-        Double sum3 = firstSide + thirdSide;
 
         try {
+            Scanner input = new Scanner(System.in);
+            System.out.println(" === Triangle Checker ===");
+            System.out.println("Input size 1: ");
+            Double firstSide = input.nextDouble();
+            System.out.println("Input size 2: ");
+            Double secondSide = input.nextDouble();
+            System.out.println("Input size 3: ");
+            Double thirdSide = input.nextDouble();
+            Double sum1 = firstSide + secondSide;
+            Double sum2 = secondSide + thirdSide;
+            Double sum3 = firstSide + thirdSide;
+
             if (firstSide < 0 || secondSide < 0 || thirdSide < 0 || sum1 <= thirdSide || sum2 <= firstSide || sum3 <= secondSide) {
-                throw new IllegalTriangleException("Input wrong!");
+                throw new IllegalTriangleException();
             } else {
                 System.out.println("3 side is accpected!");
             }
+        } catch (InputMismatchException ex) {
+            System.out.println("Wrong type!");
         } catch (IllegalTriangleException ex) {
-            System.out.println(ex.getMessage());
+            System.out.println("Wrong input!");
         }
     }
 }
